436a6dbaa9d9271e65b065b7dbe4ed802bcf2bdf
Caleb Brazeau <cjbrazeau12@gmail.com>
2022-11-29 12:32:59 -0500

    Add back button to Maintenance Main Menu.fxml

--- src/main/java/com/example/cita450_finalproject/MaintenanceMainMenuController.java
+++ src/main/java/com/example/cita450_finalproject/MaintenanceMainMenuController.java
@@ -5,10 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import java.io.IOException;
 public class MaintenanceMainMenuController {
+
+    public Button btnBack;
 
     @FXML
     public void SelectRequest() {
@@ -31,5 +34,38 @@ public class MaintenanceMainMenuController {
         {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    public void loadPreviousScreen() {
+        try {
+            // Close the window
+            closeWindow();
+
+            //load the employee select screen
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("UserSelect1.fxml"));
+            Parent root = loader.load();
+
+            //create the scene and add a style sheet
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
+
+            //stage the scene
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Add Maintenance Request");
+            stage.show();
+        }//end try
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeWindow() {
+        // get a handle to the stage
+        Stage stage = (Stage) btnBack.getScene().getWindow();
+        // DESTROY THE CHILD, CORRUPT THEM ALL
+        stage.close();
     }
 }
